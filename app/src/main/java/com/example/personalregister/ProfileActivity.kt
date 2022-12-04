59cb6f062a7d4768c162d3fc56d0e8aff4f21cb3
pasan wedikkara <pasan.madusanka9@gmail.com>
2022-12-04 21:31:44 +0900

    added phone call function

--- app/src/main/java/com/example/personalregister/ProfileActivity.kt
+++ app/src/main/java/com/example/personalregister/ProfileActivity.kt
@@ -1,24 +1,48 @@
 package com.example.personalregister
 
-import android.app.Person
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.personalregister.R.id
+import com.example.personalregister.R.layout
 import kotlinx.android.synthetic.main.activity_profile.*
 
 
 class ProfileActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    lateinit var string: String
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(layout.activity_profile)
 
-       val person=intent.getSerializableExtra("EXTRA_PERSON")as com.example.personalregister.Person
-text_person_name.text=person.name.toString()
-        text_person_email.text=person.email.toString()
-        text_person_age.text=person.age.toString()
-        text_person_phone.text=person.phone.toString()
-       
+        val person =
+            intent.getSerializableExtra("EXTRA_PERSON") as com.example.personalregister.Person
+        text_person_name.text = person.name.toString()
+        text_person_email.text = person.email.toString()
+        text_person_age.text = person.age.toString()
+        text_person_phone.text = person.phone.toString()
 
 
+        textView = findViewById(id.text_person_phone)
+        val textView: TextView = findViewById<TextView>(R.id.text_person_phone)
+
+        text_person_phone.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${text_person_phone.text}")
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+
+
+        }
     }
+
+
 }
